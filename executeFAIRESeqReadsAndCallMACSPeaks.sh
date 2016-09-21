9132ffad0eebb06db5fee1a0e1c4e595844edd3b
Spencer Nystrom <snystrom@ad.unc.edu>
2016-09-21 17:45:57 -0400

    fixed path issue in flagstats
    Former-commit-id: ff45c7de8ddcab7841245ed5826e77d2bebbc58b

--- executeFAIRESeqReadsAndCallMACSPeaks.sh
+++ executeFAIRESeqReadsAndCallMACSPeaks.sh
@@ -142,7 +142,7 @@ bamCoverage -b ${STRAIN}_q5_sorted_dupsRemoved_noYUHet.bam --numberOfProcessors 
 
 # Create collected flagstats files
 for BAM in \$(ls *.bam | cut -d. -f1); do
-	echo "${BAM}: " >> ./Flagstats/${PREFIX}_flagstats.txt
+	echo "${BAM}: " >> ../Flagstats/${PREFIX}_flagstats.txt
 	samtools flagstat ${BAM}.bam | grep -v '^0 + 0' >> ../Flagstats/${PREFIX}_flagstats.txt;
 	echo >> ../Flagstats/${PREFIX}_flagstats.txt
 done
