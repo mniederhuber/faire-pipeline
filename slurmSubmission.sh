a8c5318e52e76e6023d3bf3cc1d31beaae2fa1ea
snystrom <snystrom@ad.unc.edu>
2017-01-25 12:37:18 -0500

    Just kidding. That change breaks submission pipeline.
    Former-commit-id: 51180cc4ffdc9348934d6cf56fd5159ac1fd3936

--- slurmSubmission.sh
+++ slurmSubmission.sh
@@ -4,8 +4,6 @@
 # 2016-01-12
 # Submits snakemake pipeline to slurm cluster
 
-module load python/3.5.1
-
 pipePath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 	# This grabs the full path the submission script lies in to detect where the faire pipeline actually is on the filesystem
 	# makes it easier to tell snakemake where the Snakefile and config.json files actually are
