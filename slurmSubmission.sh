f1041640d96be5c8d9bbd1573fabad6864a8c71f
snystrom <snystrom@ad.unc.edu>
2020-12-01 13:47:58 -0500

    use envmodules submission script

--- slurmSubmission.sh
+++ slurmSubmission.sh
@@ -8,5 +8,5 @@ if [[ ! -d slurmOut ]]; then
 	mkdir slurmOut
 fi
 
-snakemake --snakefile Snakefile --cluster-config clusterConfig/slurmConfig.json --latency-wait 60 --cluster "sbatch -J {rule} -o slurmOut/slurm-%j.out -N1 -n {cluster.threads} --time {cluster.time} --mem={cluster.mem} -A {cluster.account}" --jobs 100
+snakemake --snakefile Snakefile --use-envmodules --cluster-config clusterConfig/slurmConfig.json --latency-wait 60 --cluster "sbatch -J {rule} -o slurmOut/slurm-%j.out -N1 -n {cluster.threads} --time {cluster.time} --mem={cluster.mem} -A {cluster.account}" --jobs 100
 
