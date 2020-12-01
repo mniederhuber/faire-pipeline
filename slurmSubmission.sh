e0b8db0170c790e941bdbd0cfaea86770b88c7ab
snystrom <snystrom@ad.unc.edu>
2020-12-01 11:27:36 -0500

    edit submission script to remove the $pipePath stuff, also the optarg things

--- slurmSubmission.sh
+++ slurmSubmission.sh
@@ -4,38 +4,9 @@
 # 2016-01-12
 # Submits snakemake pipeline to slurm cluster
 
-pipePath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-	# This grabs the full path the submission script lies in to detect where the faire pipeline actually is on the filesystem
-	# makes it easier to tell snakemake where the Snakefile and config.json files actually are
-	# (from http://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within)
-
-
 if [[ ! -d slurmOut ]]; then
 	mkdir slurmOut
 fi
 
-while getopts ":f:u:C" opt; do
-	case $opt in
-		f)
-		rm $(snakemake --snakefile $pipePath/Snakefile --summary | tail -n+2 | cut -f1)
-		snakemake --snakefile $pipePath/Snakefile --cluster-config $pipePath/clusterConfig/slurmConfig.json -R all --latency-wait 60 --cluster "sbatch -J {rule} -o slurmOut/slurm-%j.out -N1 -n {cluster.threads} --time {cluster.time} --mem={cluster.mem} -A {cluster.account}" --jobs 100 
-		exit
-		;;
-		u)
-		snakemake --snakefile $pipePath/Snakefile --unlock
-		exit
-		;;
-		C)
-		# take input from file named 'config' in cwd
-		snakemake --snakefile $pipePath/Snakefile --config $(cat config) --cluster-config $pipePath/clusterConfig/slurmConfig.json --latency-wait 60 --cluster "sbatch -J {rule} -o slurmOut/slurm-%j.out -N1 -n {cluster.threads} --time {cluster.time} --mem={cluster.mem} -A {cluster.account}" --jobs 100
-		exit
-		;;
-		\?)
-		echo "Invalid flag: -$OPTARG" >&2
-		exit
-		;;
-	esac
-done
-
-snakemake --snakefile $pipePath/Snakefile --cluster-config $pipePath/clusterConfig/slurmConfig.json --latency-wait 60 --cluster "sbatch -J {rule} -o slurmOut/slurm-%j.out -N1 -n {cluster.threads} --time {cluster.time} --mem={cluster.mem} -A {cluster.account}" --jobs 100
+snakemake --snakefile Snakefile --cluster-config clusterConfig/slurmConfig.json --latency-wait 60 --cluster "sbatch -J {rule} -o slurmOut/slurm-%j.out -N1 -n {cluster.threads} --time {cluster.time} --mem={cluster.mem} -A {cluster.account}" --jobs 100
 
